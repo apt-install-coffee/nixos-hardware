243a2761cdb6608c74f9950a4562f4706d884b76
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-10-19 02:27:20 +0800

    fix: add BSP release selection

--- cix/sky1/bsp/2025.09/default.nix
+++ cix/sky1/bsp/2025.09/default.nix
@@ -4,22 +4,27 @@
   config,
   ...
 }:
+let
+  cfg = config.hardware.cix.sky1;
+in
 {
-  nixpkgs.overlays = [
-    (import ./overlay.nix)
-  ];
+  config = lib.mkIf (cfg.enable && cfg.bspRelease == "2025.09") {
+    nixpkgs.overlays = [
+      (import ./overlay.nix)
+    ];
 
-  boot = {
-    kernelPackages = lib.mkOverride 900 pkgs.linuxPackages_cix;
-    extraModulePackages = with config.boot.kernelPackages; [
-      cix_gpu_kernel
-      cix_isp_driver
-      cix_npu_driver
-      cix_vpu_driver
+    boot = {
+      kernelPackages = lib.mkOverride 900 pkgs.linuxPackages_cix;
+      extraModulePackages = with config.boot.kernelPackages; [
+        cix_gpu_kernel
+        cix_isp_driver
+        cix_npu_driver
+        cix_vpu_driver
+      ];
+    };
+
+    hardware.firmware = [
+      pkgs.cix_vpu_firmware
     ];
   };
-
-  hardware.firmware = [
-    pkgs.cix_vpu_firmware
-  ];
 }
