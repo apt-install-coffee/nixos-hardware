23fd30a0852e54eb98202ed322a887fde59fd10d
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-10-19 00:46:53 +0800

    hack: would this work?

--- cix/sky1/kernel/overlay.nix
+++ cix/sky1/kernel/overlay.nix
@@ -4,12 +4,14 @@ final: _prev: {
       linux_6_6_89 = final.callPackage ./linux-6.6.89.nix {};
     };
     vanillaPackages = _prev.linuxKernel.vanillaPackages // {
-      linux_6_6_89 = final.recurseIntoAttrs ((final.linuxKernel.packagesFor final.linuxKernel.kernels.linux_6_6_89) // {
-        cix_gpu_kernel = final.linuxKernel.packages.linux_6_6_89.callPackage ./modules/cix_gpu_kernel { };
-        cix_isp_driver = final.linuxKernel.packages.linux_6_6_89.callPackage ./modules/cix_isp_driver { };
-        cix_npu_driver = final.linuxKernel.packages.linux_6_6_89.callPackage ./modules/cix_npu_driver { };
-        cix_vpu_driver = final.linuxKernel.packages.linux_6_6_89.callPackage ./modules/cix_vpu_driver { };
-      });
+      linux_6_6_89 = final.recurseIntoAttrs ((final.linuxKernel.packagesFor final.linuxKernel.kernels.linux_6_6_89).extend (
+        _: previous: {
+          cix_gpu_kernel = final.linuxKernel.packages.linux_6_6_89.callPackage ./modules/cix_gpu_kernel { };
+          cix_isp_driver = final.linuxKernel.packages.linux_6_6_89.callPackage ./modules/cix_isp_driver { };
+          cix_npu_driver = final.linuxKernel.packages.linux_6_6_89.callPackage ./modules/cix_npu_driver { };
+          cix_vpu_driver = final.linuxKernel.packages.linux_6_6_89.callPackage ./modules/cix_vpu_driver { };
+        }
+      ));
     };
   };
   linuxPackages_6_6_89 = final.linuxKernel.packages.linux_6_6_89;
