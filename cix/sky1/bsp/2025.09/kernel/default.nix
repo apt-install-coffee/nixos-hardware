a7243de3e957787327cb2d42617ee36845f7b08f
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-10-19 19:20:05 +0800

    fix: support O6N

--- cix/sky1/bsp/2025.09/kernel/default.nix
+++ cix/sky1/bsp/2025.09/kernel/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  fetchurl,
   fetchFromGitHub,
   buildLinux,
   ...
@@ -23,6 +24,34 @@ let
       };
 
       kernelPatches = [
+        {
+          name = "0001-DPTSW-16669-0-clocksource-timer-sky1-gpt-remove-rese";
+          patch = fetchurl {
+            url = "https://github.com/radxa-pkg/linux-sky1/raw/refs/heads/main/debian/patches/cix/wip/0001-DPTSW-16669-0-clocksource-timer-sky1-gpt-remove-rese.patch";
+            sha256 = "sha256-MdyOe1tsjDHEnI0zq6Qf+ZOF30ljl9Gk3BWdWtmbivs=";
+          };
+        }
+        {
+          name = "0002-DPTSW-16669-1-pwm-remove-reset-operation";
+          patch = fetchurl {
+            url = "https://github.com/radxa-pkg/linux-sky1/raw/refs/heads/main/debian/patches/cix/wip/0002-DPTSW-16669-1-pwm-remove-reset-operation.patch";
+            sha256 = "sha256-1nW/3eXV061Elh++W78V0AGeskyashgz2fyS0EQAGLE=";
+          };
+        }
+        {
+          name = "0003-DPTSW-16669-2-arch-arm64-dts-cix-disable-uart1";
+          patch = fetchurl {
+            url = "https://github.com/radxa-pkg/linux-sky1/raw/refs/heads/main/debian/patches/cix/wip/0003-DPTSW-16669-2-arch-arm64-dts-cix-disable-uart1.patch";
+            sha256 = "sha256-RInyObRL1afEf9XBj8jMtDcFYYKS3s1n8kt0YeGRopo=";
+          };
+        }
+        {
+          name = "0004-DPTSW-17177-pwm-sky1-remove-pwm-clock-auto-enable-fe";
+          patch = fetchurl {
+            url = "https://github.com/radxa-pkg/linux-sky1/raw/refs/heads/main/debian/patches/cix/wip/0004-DPTSW-17177-pwm-sky1-remove-pwm-clock-auto-enable-fe.patch";
+            sha256 = "sha256-E3JWI2HM0YTsXvK7hjvRtAdvp6yga4XjAfjtvShZK4c=";
+          };
+        }
         {
           name = "fix-fwnode_regulator";
           patch = ./fix-fwnode_regulator.patch;
