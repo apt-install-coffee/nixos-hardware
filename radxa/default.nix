d1fdbac4844a762954cf7be843b499ff6ae9ae68
ZHANG Yuntian <yt@radxa.com>
2025-10-24 17:08:46 +0800

    feat: add description for radxa binary cache
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>

--- radxa/default.nix
+++ radxa/default.nix
@@ -15,12 +15,27 @@ in
 
   options.hardware.radxa = {
     enable = lib.mkEnableOption "Radxa system support";
+    cachix.enable = lib.mkEnableOption ''
+      Radxa Cachix binary cache.
+
+      This is a runtime option. If you are cross building system images, you
+      need to run `cachix use radxa` on your build machine.
+    '';
   };
 
   config = lib.mkIf cfg.enable {
     boot = {
       kernelPackages = lib.mkOverride 990 pkgs.linuxPackages_latest;
       loader.systemd-boot.enable = lib.mkDefault true;
+    };
+
+    nix.settings = lib.mkIf cfg.cachix.enable {
+      substituters = [
+        "https://radxa.cachix.org"
+      ];
+      trusted-public-keys = [
+        "radxa.cachix.org-1:Jc5T8fpq3URBLeKKHER2PxcuAd74iPMiW6TOb1M1yPc="
+      ];
     };
   };
 }
