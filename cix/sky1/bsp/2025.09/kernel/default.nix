af1977eeab0a62225da8ce1a66117d0f039df9b5
ZHANG Yuntian <yt@radxa.com>
2025-12-15 18:06:10 +0800

    fix: update patch hash
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>

--- cix/sky1/bsp/2025.09/kernel/default.nix
+++ cix/sky1/bsp/2025.09/kernel/default.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  fetchurl,
+  fetchpatch2,
   fetchFromGitHub,
   buildLinux,
   ...
@@ -26,37 +26,37 @@ let
       kernelPatches = [
         {
           name = "0001-DPTSW-16669-0-clocksource-timer-sky1-gpt-remove-rese";
-          patch = fetchurl {
+          patch = fetchpatch2 {
             url = "https://github.com/radxa-pkg/linux-sky1/raw/99dae262da2185f18503720328b42c4c244a1a0d/debian/patches/cix/wip/0001-DPTSW-16669-0-clocksource-timer-sky1-gpt-remove-rese.patch";
-            hash = "sha256-tUNGipYogpTsX0Sc290avZAtZGCyjVz4MhyNiOzY51c=";
+            hash = "sha256-4H5wZU9UGa+NXLy1oMCzESYayjCfE5g6TA2fNarMpRU=";
           };
         }
         {
           name = "0002-DPTSW-16669-1-pwm-remove-reset-operation";
-          patch = fetchurl {
+          patch = fetchpatch2 {
             url = "https://github.com/radxa-pkg/linux-sky1/raw/99dae262da2185f18503720328b42c4c244a1a0d/debian/patches/cix/wip/0002-DPTSW-16669-1-pwm-remove-reset-operation.patch";
-            hash = "sha256-Gk+Gjz6ZcixN3mrxnEJIbp0zBHbc7C3zDzSOyLBTHmI=";
+            hash = "sha256-bAIyps9FFdC4yzcyqja0ZyhEyJOt3ivajXilSITvmUw=";
           };
         }
         {
           name = "0003-DPTSW-16669-2-arch-arm64-dts-cix-disable-uart1";
-          patch = fetchurl {
+          patch = fetchpatch2 {
             url = "https://github.com/radxa-pkg/linux-sky1/raw/99dae262da2185f18503720328b42c4c244a1a0d/debian/patches/cix/wip/0003-DPTSW-16669-2-arch-arm64-dts-cix-disable-uart1.patch";
-            hash = "sha256-ctGReQu8RonZIp5v7c1LnIGDBGXPXXFJiDKV+DcUjhs=";
+            hash = "sha256-BcQt4KwhjdRvYpo7EDFROGAv+9JbBHiM1tK2RpmpCkk=";
           };
         }
         {
           name = "0004-DPTSW-17177-pwm-sky1-remove-pwm-clock-auto-enable-fe";
-          patch = fetchurl {
+          patch = fetchpatch2 {
             url = "https://github.com/radxa-pkg/linux-sky1/raw/99dae262da2185f18503720328b42c4c244a1a0d/debian/patches/cix/wip/0004-DPTSW-17177-pwm-sky1-remove-pwm-clock-auto-enable-fe.patch";
-            hash = "sha256-kmOy+lsQosv176rKZ35qvdhsMk7Oe9k1kn7ZdvyN4c4=";
+            hash = "sha256-feY75Q3DTt8sh8Swia6aq+wumfmHzrmkTFjkgXeHrZw=";
           };
         }
         {
           name = "drm/panthor: Add ACPI support";
-          patch = fetchurl {
+          patch = fetchpatch2 {
             url = "https://gitlab.com/cix-linux/cix_opensource/linux/-/merge_requests/1.patch";
-            hash = "sha256-KhYsffNvqdOCaJi8oGP915HQ4za8RvEXlRseFkwhhWE=";
+            hash = "sha256-XtMIDW/Uw7/yzUFW9BbpaZlfoxCxynzQenNTurL9c/E=";
           };
         }
       ];
