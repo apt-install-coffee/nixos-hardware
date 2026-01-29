fa4e7f911f9e7f82ce22a302abe6e2dc8db061a2
apt-install-coffee <jordan.mccallum@mailbox.org>
2026-01-29 15:01:06 +1000

    sky1: allow kernelPatches passthrough

--- cix/sky1/bsp/sky1-linux/overlay.nix
+++ cix/sky1/bsp/sky1-linux/overlay.nix
@@ -1,19 +1,21 @@
 final: _prev: {
   linuxKernel = _prev.linuxKernel // {
     kernels = _prev.linuxKernel.kernels // {
-      linux_6_18_2 = final.callPackage ./kernel {};
+      linux_6_18_2 = final.callPackage ./kernel { };
     };
     vanillaPackages = _prev.linuxKernel.vanillaPackages // {
-      linux_6_18_2 = final.lib.recurseIntoAttrs ((final.linuxKernel.packagesFor final.linuxKernel.kernels.linux_6_18_2).extend (
-        _: _: {
-          cix_npu_driver = final.linuxKernel.packages.linux_6_18_2.callPackage ./modules/cix_npu_driver { };
-          cix_vpu_driver = final.linuxKernel.packages.linux_6_18_2.callPackage ./modules/cix_vpu_driver { };
-        }
-      ));
+      linux_6_18_2 = final.lib.recurseIntoAttrs (
+        (final.linuxKernel.packagesFor final.linuxKernel.kernels.linux_6_18_2).extend (
+          _: _: {
+            cix_npu_driver = final.linuxKernel.packages.linux_6_18_2.callPackage ./modules/cix_npu_driver { };
+            cix_vpu_driver = final.linuxKernel.packages.linux_6_18_2.callPackage ./modules/cix_vpu_driver { };
+          }
+        )
+      );
     };
   };
   linuxPackages_6_18_2 = final.linuxKernel.packages.linux_6_18_2;
   linux_6_18_2 = final.linuxKernel.kernels.linux_6_18_2;
 
-  sky1-firmware = final.callPackage ./firmwares/sky1-firmware {};
+  sky1-firmware = final.callPackage ./firmwares/sky1-firmware { };
 }
